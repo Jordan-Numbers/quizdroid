b6e1f8070d37853f3450b4872f6bb25cb318f811
Jordan Smith <j75smith@uw.edu>
2015-05-19 14:31:08 -0700

    Preferences don't update and toast doesnt fire

--- app/src/main/java/edu/washington/j75smith/quizdroid/MainActivity.java
+++ app/src/main/java/edu/washington/j75smith/quizdroid/MainActivity.java
@@ -1,25 +1,39 @@
 package edu.washington.j75smith.quizdroid;
 
-import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
 
 import java.util.List;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends ActionBarActivity {
 
+    private String source;
+    private PendingIntent alarmIntent = null;
+    private BroadcastReceiver alarmReceiver;
+    private AlarmManager manager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //listen for a topic to be selected
-        QuizApp app = (QuizApp) getApplication();
 
+        //get the QuizApp and domain topics
+        QuizApp app = (QuizApp) getApplication();
         List<topic> topics = app.getAllTopics();
 
         //get the buttons
@@ -37,6 +51,63 @@ public class MainActivity extends Activity {
         btn_one.setOnClickListener(listener);
         btn_two.setOnClickListener(listener);
         btn_three.setOnClickListener(listener);
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        int interval;
+
+        //get prefs
+        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+
+        //define defaults
+        String defSource = (String) getText(R.string.source_one);
+        int defTime = 600;
+
+        if(sharedPref.getAll() == null){
+            //user has no prefs
+            //set to defaults
+
+            //locally
+            this.source = defSource;
+            interval = defTime;
+
+            //and in prefs
+            SharedPreferences.Editor editor = sharedPref.edit();
+            editor.putInt("when", interval);
+            editor.putString("URl", this.source);
+            editor.apply();
+
+        } else {
+            //get the users preferred update
+            this.source = sharedPref.getString("URL", defSource);
+            interval = sharedPref.getInt("when", defTime);
+        }
+
+        //Get the manager
+       this.manager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+
+        //init our receiver
+        this.alarmReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                Toast.makeText(MainActivity.this, source, Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        //register our receiver
+        registerReceiver(this.alarmReceiver, new IntentFilter("edu.washington.j75smith.quizdroid"));
+
+        //make the pending intent and its intent
+        Intent stuffing = new Intent();
+        stuffing.setAction("edu.washington.j75smith.awty");
+        alarmIntent = PendingIntent.getBroadcast(this, 0, stuffing, 0);
+
+        int millis = Math.round(interval * 60000);
+        manager.setRepeating(AlarmManager.RTC, System.currentTimeMillis() + millis, millis, alarmIntent);
+
+        Log.i("QUIZDROID_onStart", "interval " + interval + " URL " + this.source);
     }
 
     class mainOnClickListener implements OnClickListener {
@@ -73,5 +144,37 @@ public class MainActivity extends Activity {
             next.putExtra("topic", selectedTopic);
             startActivity(next);
         }
-    };
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_prefs) {
+            Intent pref = new Intent(MainActivity.this, PreferencesActivity.class);
+            startActivity(pref);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        //stop sending
+        manager.cancel(alarmIntent);
+        alarmIntent.cancel();
+        unregisterReceiver(this.alarmReceiver);
+    }
 }
---
+++ app/src/main/java/edu/washington/j75smith/quizdroid/PreferencesActivity.java
@@ -0,0 +1,59 @@
+package edu.washington.j75smith.quizdroid;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.content.SharedPreferences;
+
+
+public class PreferencesActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preferences);
+        //get refs to user input
+        RadioGroup radio_sources = (RadioGroup) findViewById(R.id.sources);
+        EditText interval = (EditText) findViewById(R.id.when);
+
+        //listen for go-ahead to apply changes
+        findViewById(R.id.btn_apply).setOnClickListener(
+                                    new ApplyOnClickListener(radio_sources, interval));
+    }
+
+    class ApplyOnClickListener implements View.OnClickListener {
+
+        private RadioGroup radio_sources;
+        private EditText interval;
+
+        ApplyOnClickListener(RadioGroup radio_sources, EditText interval){
+            this.radio_sources = radio_sources;
+            this.interval = interval;
+        }
+
+        @Override
+        public void onClick(View v) {
+            RadioButton checked = (RadioButton) this.radio_sources
+                                   .getChildAt(this.radio_sources.getCheckedRadioButtonId());
+            if(checked == null){
+                checked = (RadioButton) this.radio_sources.getChildAt(0);
+            }
+            String source = (String) checked.getText();
+            int minutes = Integer.parseInt(this.interval.getText().toString());
+
+            SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPref.edit();
+            editor.putInt("when", minutes);
+            editor.putString("URl", source);
+            editor.apply();
+
+            Intent update = new Intent(PreferencesActivity.this, MainActivity.class);
+            startActivity(update);
+        }
+    }
+}
